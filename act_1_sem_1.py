dd8fc92c284d0645e7de96e6d6a59ee6c9dfbf0e
WIlfrido J. Paredes <47059111+Wilfridovich17@users.noreply.github.com>
2021-02-12 08:46:42 -0600

    Create act_1_sem_1.py

---
+++ act_1_sem_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+lista = [34,478,233,13]
+lista_random = list(np.array(np.random.rand(50)*100).astype(int))
+
+def my_sort(data_test):      
+    for i in range(len(data_test)):
+        for j in range(i+1,len(data_test)):
+            if data_test[i] > data_test[j]:
+                a = data_test[i]
+                b = data_test[j]
+                data_test[i] = b
+                data_test[j] = a
+    
+    return data_test
+
+print(my_sort(lista))
+print(my_sort(lista_random))
