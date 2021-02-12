e28bbabe7379d4d4855380f955ecead0611f711c
WIlfrido J. Paredes <47059111+Wilfridovich17@users.noreply.github.com>
2021-02-12 13:00:06 -0600

    Update act_1_sem_1.py
    Se acorta la asignación

--- act_1_sem_1.py
+++ act_1_sem_1.py
@@ -7,11 +7,7 @@ def my_sort(data_test):
     for i in range(len(data_test)):
         for j in range(i+1,len(data_test)):
             if data_test[i] > data_test[j]:
-                a = data_test[i]
-                b = data_test[j]
-                data_test[i] = b
-                data_test[j] = a
-    
+                data_test[i],data_test[j] = data_test[j], data_test[i]     
     return data_test
 
 print(my_sort(lista))
